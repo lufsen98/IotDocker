9f458d25ab407d18083ab681610ebe131308bdb3
Ludde <luddenorin@gmail.com>
2025-02-03 22:13:24 +0100

    Added arp network scan port scan

---
+++ bad/arp.py
@@ -0,0 +1,10 @@
+from scapy.all import sniff, ARP
+
+def arp_display(pkt):
+    if pkt.haslayer(ARP):
+        if pkt[ARP].op == 1:
+            return f"Request: {pkt[ARP].psrc} is asking about {pkt[ARP].pdst}"
+        if pkt[ARP].op == 2:
+            return f"*Response: {pkt[ARP].hwsrc} has adress{pkt[ARP].psr}"
+print("Sniffing ARP packets ...")
+sniff(prn=arp_display, filter="arp", store=0, count=0)
